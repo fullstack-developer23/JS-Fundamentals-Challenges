cc814a217fb1672f965c7b75ed9161e3d9fc9418
fullstack-developer-23 <ahmadsafeer1997@gmail.com>
2023-11-12 23:45:41 +0000

    code uncommented now

--- Functions.js
+++ Functions.js
@@ -1,39 +1,39 @@
 //Activity 1
 
-// const factorial = (n) =>{
-//     if((n===0) || (n===1))
-//     {
-//         return 1;
-//     }
-//     else
-//     {
-//         return (n * factorial(n-1));
-//     }
-// }
+const factorial = (n) =>{
+    if((n===0) || (n===1))
+    {
+        return 1;
+    }
+    else
+    {
+        return (n * factorial(n-1));
+    }
+}
 
-// console.log(factorial(33));
+console.log(factorial(33));
 
 //Activity 2
 
-// const takeOrder = (topping, orderCount = 0) =>{
-//     console.log(`Order number ${orderCount} is Pizza with ${topping} `);
-//     orderCount++;
-// }
+const takeOrder = (topping, orderCount = 0) =>{
+    console.log(`Order number ${orderCount} is Pizza with ${topping} `);
+    orderCount++;
+}
 
-// takeOrder("onion and chillies", 1);
+takeOrder("onion and chillies", 1);
 
 //Activity 3
 
-// let safeerBalance = 2000;
-// let withdrawAmount = 2000;
-// let safeerPin = 12345;
-// let pinEntered = 12345;
+let safeerBalance = 2000;
+let withdrawAmount = 2000;
+let safeerPin = 12345;
+let pinEntered = 12345;
 
-// if(pinEntered == safeerPin && withdrawAmount >= safeerBalance)
-// {
-//     console.log("Congratulations! Your cash is dispensed.");
-// }
-// else
-// {
-//     console.log("Sorry! Your cash cannot be dispensed.");
-// }
+if(pinEntered == safeerPin && withdrawAmount >= safeerBalance)
+{
+    console.log("Congratulations! Your cash is dispensed.");
+}
+else
+{
+    console.log("Sorry! Your cash cannot be dispensed.");
+}
